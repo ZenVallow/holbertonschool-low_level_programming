0aec6e6b599e5f3ad4e6a2d02ab68ae9408e6a22
ZenVallow <mathys.thomas@hotmail.com>
2023-06-19 06:07:30 -0700

    add Last digit of too printf

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,17 +16,17 @@ int main(void)
 	/* your code goes there */
 	if (n > 5)
 	{
-		printf("%d and is greater than 5\n", n);
+		printf("Last digit of %d and is greater than 5\n", n);
 	}
 
 	else if (n == 0)
 	{
-		printf("%d and is 0\n", n);
+		printf("Last digit of %d and is 0\n", n);
 	}
 
 	else if (n != 6 && n != 0)
 	{
-		printf("%d and it's less than 6 and not 0\n", n);
+		printf("Last digit of %d and it's less than 6 and not 0\n", n);
 	}
 	return (0);
 }
