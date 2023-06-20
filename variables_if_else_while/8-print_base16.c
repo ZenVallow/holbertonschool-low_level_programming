6fc38997318d3d9ebb2dd06a6a27a4b27b5e0253
ZenVallow <mathys.thomas@hotmail.com>
2023-06-20 04:03:37 -0700

    add hexadecimal c programm

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	char c, d;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (d = 'a'; d <= 'f'; d++)
+	{
+		putchar (d);
+	}
+	{
+	putchar('\n');
+	}
+	return (0);
+}
