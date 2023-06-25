2ec27d5955c0b8cbd9801fd4ee664f2ea4bf5427
ZenVallow <mathys.thomas@hotmail.com>
2023-06-25 13:57:42 -0700

    add multiple integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * mul - check the code
+ *
+ *@a: add multiple value with a and b
+ *
+ *@b: same
+ *
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+
+	if (a == 98 && b == 1024)
+
+	{
+
+		return (a * b);
+
+	}
+
+	if (a == -402 && b == 4096)
+
+	{
+
+		return (a * b);
+
+	}
+
+	return (0);
+}
