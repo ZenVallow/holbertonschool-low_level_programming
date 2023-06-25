57d0cddd0ab82ef86c054d52e6a21ca55d02abbb
ZenVallow <mathys.thomas@hotmail.com>
2023-06-25 12:08:23 -0700

    Multiplication table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - check the code
+ *
+ *
+ * Return: void.
+ */
+
+void times_table(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			if (y * x > 9)
+				_putchar(y * x / 10 + '0');
+			else if (y > 0)
+				_putchar(' ');
+			_putchar(y * x % 10 + '0');
+			if (y < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
