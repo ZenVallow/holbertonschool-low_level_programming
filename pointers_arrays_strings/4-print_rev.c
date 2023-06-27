3aba8465b880bac0f2ffb52d01d1e998697d1b5b
ZenVallow <mathys.thomas@hotmail.com>
2023-06-27 05:03:59 -0700

    reverse printer

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ *
+ *@s: reverse
+ *
+ * Return: void.
+ */
+
+void print_rev(char *s)
+{
+	char *rev = s;
+
+	while (*rev != '\0')
+	{
+		rev++;
+	}
+	while (rev >= s)
+	{
+		_putchar(*rev);
+		rev--;
+	}
+	_putchar('\n');
+}
