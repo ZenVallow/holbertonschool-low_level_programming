11ac527626ad6cbc976847d5a385ec387466fbb1
ZenVallow <mathys.thomas@hotmail.com>
2023-06-28 02:02:50 -0700

    fix betty

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,9 +2,11 @@
 #include "2-strlen.c"
 
 /**
- * main - check the code
+ * puts_half - check the code
  *
- * Return: Always 0.
+ *@str: string.
+ *
+ * Return: void.
  */
 
 void puts_half(char *str)
@@ -13,13 +15,13 @@ void puts_half(char *str)
 	int index = 0;
 	int length = _strlen(str);
 
-
-        if (length % 2 == 1)
+	if (length % 2 == 1)
 		length--;
 
 	while (str[index] != '\0')
+
 	{
-		if (index >= length /2)
+		if (index >= length / 2)
 		_putchar(str[index]);
 		index++;
 	}
