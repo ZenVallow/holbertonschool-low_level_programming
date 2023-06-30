5cbca895d4cee40411142d7d864553e3eda456d5
ZenVallow <mathys.thomas@hotmail.com>
2023-06-30 00:52:02 -0700

    fix betty

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,32 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * _strncpy - check the code
  *
- * Return: Always 0.
+ * Return: dest.
+ *
+ *@dest: destination.
+ *
+ *@src: source.
+ *
+ *@n: integer.
  */
 
 char *_strncpy(char *dest, char *src, int n)
+{
 
-{       int index = 0;
+	int index = 0;
 
 	for (index = 0; index < n && src[index] != '\0'; index++)
-		{
+	{
 
-			dest[index] = src[index];
-		}
+		dest[index] = src[index];
+	}
 
-        for (; index < n; index++)
+	for (; index < n; index++)
 	{
 		dest[index] = '\0';
 	}
 
-	return dest;
+	return (dest);
 }
