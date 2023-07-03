b22e22dc5085fda95f0f4210e690360ea06c3f3f
ZenVallow <mathys.thomas@hotmail.com>
2023-07-03 07:35:03 -0700

    add strlen recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - check the code
+ *
+ *@s: string.
+ *
+ * Return: Always 0 or 1.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
