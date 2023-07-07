18b5d15224af2c378a16a569ba895d1dfcf0982f
ZenVallow <mathys.thomas@hotmail.com>
2023-07-07 03:11:33 -0700

    add task 1 and 2

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - check the code
+ *
+ *@argc: use for print the numbers of parameters.
+ *
+ *
+ * Return - 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)(argv);
+	printf("%d\n", argc - 1);
+	return 0;
+}
