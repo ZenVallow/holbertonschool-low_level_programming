d8ec0f9dc6b3e8a2443835b44b8abbdf01dd9a25
ZenVallow <mathys.thomas@hotmail.com>
2023-07-17 04:56:22 -0700

    initialization of struct dog variable

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize dog structure
+ *
+ *@d: pointer to the struct dog variable
+ *
+ *@name: name of the dog
+ *
+ *@age: age of the dog
+ *
+ *@owner: owner of the dog
+ *
+ * Return: void.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -22,3 +22,7 @@ char *owner;
 } dog;
 
 #endif /* DOG_H */
+
+#include <stdio.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
