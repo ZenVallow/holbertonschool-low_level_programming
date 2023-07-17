7f95d116d23b9871ce3b3df1c75d3081830b3153
ZenVallow <mathys.thomas@hotmail.com>
2023-07-17 03:08:15 -0700

    fix com for betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - struct for print information of dog.
+ *
+ *@name: name of the dog.
+ *
+ *@age: age of the dog.
+ *
+ *@owner: owner of the dog.
+ *
+ **/
 
 typedef struct dog
+
 {
 char *name;
+
 float age;
+
 char *owner;
 } dog;
 
