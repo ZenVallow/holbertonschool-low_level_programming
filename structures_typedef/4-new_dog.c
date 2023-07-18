f08ba4299212650543be777a131e78700c089ee0
ZenVallow <mathys.thomas@hotmail.com>
2023-07-18 00:45:11 -0700

    fix betty

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,14 +3,22 @@
 #include "dog.h"
 
 /**
- * main - check the code
+ * new_dog - creation of new dog
  *
- * Return: Always 0.
+ *@name: pointer for the name of the dog
+ *
+ *@age: age of the dog
+ *
+ *@owner: pointer to the owner of the dog
+ *
+ *
+ * Return: dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog = malloc(sizeof(dog_t));
+
 	if (dog == NULL)
 	{
 		return (NULL);
