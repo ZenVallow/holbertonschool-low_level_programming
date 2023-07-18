6f81c14d51ecb22d265cb93beba4e4e7085b2c5f
ZenVallow <mathys.thomas@hotmail.com>
2023-07-18 05:22:46 -0700

    add function who print a name with a pointer

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ *
+ * @name: name of the person
+ *
+ *@f: pointer of function
+ *
+ * Return: void.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+	{
+		return;
+	}
+
+	(*f)(name);
+}
