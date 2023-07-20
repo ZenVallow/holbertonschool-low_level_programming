6653d6d779a8273428b1ae0eba683dbedf5b11f5
ZenVallow <mathys.thomas@hotmail.com>
2023-07-20 13:05:09 +0200

    change for loop for while loop

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -21,14 +21,15 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 
-	int i;
+	int i = 0;
 
-	for (i = 0; ops[i].op != NULL; i++)
+	while(ops[i].op != NULL)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 		{
 			return (ops[i].f);
 		}
+		i++;
 	}
 
 	return (NULL);
