2f159469e4cb50a1147350bf0b2a8ed2c104376b
ZenVallow <mathys.thomas@hotmail.com>
2023-07-21 21:54:44 +0200

    Function who print anything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,73 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - Function who print anything
+ *
+ *@format: array for print different types of characters or integers.
+ *
+ *
+ *
+ *
+ *
+ *Return: void.
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+
+	char c;
+	int index, i;
+	float f;
+	char *s;
+
+	va_start(args, format);
+
+	while (format && format[index])
+	{
+		i = 0;
+
+		switch(format[index])
+		{
+			case 'c':
+			c = va_arg(args, int);
+			printf("%c", c);
+			break;
+
+			case 'i':
+			i = va_arg(args, int);
+			printf("%d", i);
+			break;
+
+			case 'f':
+			f = va_arg(args, double);
+			printf("%f", f);
+			break;
+
+			case 's':
+			s = va_arg(args, char*);
+			if (s == NULL)
+			{
+				printf("(nil)");
+			}
+
+			printf("%s" , s);
+
+			break;
+
+		}
+		if (format[index + 1] != '\0' && (format[index] == 'c' ||
+		format[index] == 'i' || format[index] == 'f' || format[index] == 's'))
+
+			printf(", ");
+
+		index++;
+
+	}
+
+	printf("\n");
+	va_end(args);
+
+}
