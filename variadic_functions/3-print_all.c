7bf6dc767598d26738f7f900abf792f2822baedd
ZenVallow <mathys.thomas@hotmail.com>
2023-07-21 21:59:55 +0200

    fix space betty

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -7,67 +7,49 @@
  *
  *@format: array for print different types of characters or integers.
  *
- *
- *
- *
- *
  *Return: void.
  */
 
 void print_all(const char * const format, ...)
 {
 	va_list args;
-
 	char c;
 	int index, i;
 	float f;
 	char *s;
 
 	va_start(args, format);
-
 	while (format && format[index])
 	{
 		i = 0;
-
-		switch(format[index])
+		switch (format[index])
 		{
 			case 'c':
 			c = va_arg(args, int);
 			printf("%c", c);
 			break;
-
 			case 'i':
 			i = va_arg(args, int);
 			printf("%d", i);
 			break;
-
 			case 'f':
 			f = va_arg(args, double);
 			printf("%f", f);
 			break;
-
 			case 's':
 			s = va_arg(args, char*);
 			if (s == NULL)
 			{
 				printf("(nil)");
 			}
-
-			printf("%s" , s);
-
+			printf("%s", s);
 			break;
-
 		}
 		if (format[index + 1] != '\0' && (format[index] == 'c' ||
 		format[index] == 'i' || format[index] == 'f' || format[index] == 's'))
-
 			printf(", ");
-
 		index++;
-
 	}
-
 	printf("\n");
 	va_end(args);
-
 }
